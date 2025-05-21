9b3e32a562097510298f7e447c7b779e5b3f8dc5
SanskarJain1009 <sanskarjain1009@gmail.com>
2025-05-21 17:23:33 +0530

    Sum of N Factorial Loop Function

--- sum_of_n_factorial.c
+++ sum_of_n_factorial.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+int sum(int n){
+    int sum = 0;
+    for(int i = 1; i<= n; i++)
+    {
+        int fac = 1;
+        for(int j= i;j>0;j--)
+        {
+            fac = fac * j;
+        }
+        sum = sum + fac;
+    }
+    return sum;
+}
+
+int main()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    printf("Output: %d", sum(n));
+    return 0;
+}
